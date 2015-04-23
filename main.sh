5b6fdb823bec7cf7cd2f72585d1d37872c034d66
Sam Norbury <sam.norbury@ukfast.co.uk>
2015-04-23 16:22:53 +0200

    far more tidy, removed most of the junk. Added yum/apt patch install

--- main.sh
+++ main.sh
@@ -1,49 +1,28 @@
-#disclaimer
-#get patches from github or offer to use local files if they don't trust.
-#apt/yum install patch
-#find unpatches sites
-#apply patch to site from docroot (as root or as site user? test)
-#scan once more for unpatches sites
-#echo out something about clearing caches - http://www.magentocommerce.com/knowledge-base/entry/cache-storage-management/%09200
-#https://shoplift.byte.nl/
-#FIN
-
-##############
-## SCRATCH
-##############
-
-#path location
-#patches/$VERSION.sh
-
-#find mage versions
-#for i in $(find / -wholename '*/app/Mage.php'); do echo "$i <-> `php -r ";require '$i'; echo Mage::getVersion(); "`"; done
-
-#find unpatched sites
-#find / -wholename '*/app/code/core/Mage/Core/Controller/Request/Http.php' -exec grep -L _internallyForwarded {} \;
-
-#apply patch - pseudo
-
-#$UNPATCHEDSITES = awk -F'app/code/core' {'print $1'} $UNPATCHEDSITES
-
-#$VERSION = for i in $UNPATCHEDSITES; do php -r ";require '$i/app/Mage.php'; echo Mage::getVersion(); "; done
-
-#for i in $UNPATCHEDSITES; do cp patches/$VERSION $UNPATCHEDSITES && bash $UNPATCHEDSITES/$VERSION.sh ; done
-
-#1.4.0-1.5.0.sh
-#1.5.1.sh
-#1.6.0.sh
-#1.6.1-1.6.2.sh
-#1.7.sh
-#1.8-1.9.sh
-
-##############
-## END SCRATCH
-##############
+#magento autopatcher
 
 
+#Install needed packages
+
+YUM=$(which yum)
+APT=$(which apt-get)
+
+if [ -z "$YUM" ]
+then
+  #no yum so presume apt
+  apt-get install patch
+elif [ -z "$APT" ]
+then
+  #no apt so presume yum. Bit crude
+  yum -y install patch
+fi
 
 #wget the patches from git to /tmp/magepatches
 
+wget -O /tmp/magepatches/1.4.0-1.5.0.sh  https://raw.githubusercontent.com/zyio/magento-shoplift-autopatcher/master/patches/1.4.0-1.5.0.sh
+wget -O /tmp/magepatches/1.5.1.sh  https://raw.githubusercontent.com/zyio/magento-shoplift-autopatcher/master/patches/1.5.1.sh
+wget -O /tmp/magepatches/1.6.0.sh  https://raw.githubusercontent.com/zyio/magento-shoplift-autopatcher/master/patches/1.6.0.sh
+wget -O /tmp/magepatches/1.6.1-1.6.2.sh  https://raw.githubusercontent.com/zyio/magento-shoplift-autopatcher/master/patches/1.6.1-1.6.2.sh
+wget -O /tmp/magepatches/1.7.sh  https://raw.githubusercontent.com/zyio/magento-shoplift-autopatcher/master/patches/1.7.sh
 
 #Find all the vulns!
 for SITE in $(find / -path '*/app/code/core/Mage/Core/Controller/Request/Http.php' -exec grep -L _internallyForwarded {} \;); do
@@ -84,23 +63,13 @@ for SITE in $(find / -path '*/app/code/core/Mage/Core/Controller/Request/Http.ph
   OWNER=$(stat -c '%U' app/Mage.php)
   sudo -u${OWNER} /bin/bash ${PATCH}
 
-
-
   echo -e "Magento installation in ${DOCROOT} patched\n"
 
 done
+
+rm -rf /tmp/magepatches
 
 echo -e "Now clear your caches out:\n \
       - https://www.byte.nl/wiki/How_to_apply_Magento_patch_SUPEE-5344?_ga=1.122262091.1191872873.1429777915\n \
       - Restart your webserver and/or php-fpm to be sure\n \
       - Consider apc/opcache as well"
-
-
-
-
-#determine version of site and patch appropriately
-#https://www.byte.nl/wiki/How_to_apply_Magento_patch_SUPEE-5344?_ga=1.122262091.1191872873.1429777915
-
-
-
-
