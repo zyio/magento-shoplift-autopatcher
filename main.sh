c978005cba42d76a533488a4bb866dc7d150ac33
Sam Norbury <sam.norbury@ukfast.co.uk>
2015-04-24 11:46:29 +0200

    fixing stupid awk cockups

--- main.sh
+++ main.sh
@@ -28,37 +28,40 @@ wget -qO /tmp/magepatches/1.7.sh  https://raw.githubusercontent.com/zyio/magento
 #Find all the vulns!
 for SITE in $(find / -path '*/app/code/core/Mage/Core/Controller/Request/Http.php' -exec grep -L _internallyForwarded {} \;); do
  
-  #Find Mage version of said site
-  VERSION=$(php -r ";require '${SITE}/app/Mage.php'; echo Mage::getVersion(); "|tr -d'.')
-
   #Determine document root with a questionable awk
-  DOCROOT=$(awk -F'app/code/core' {'print $1'} $SITE)
+  #DOCROOT=$(awk -F'app/code/core' {'print $1'} $SITE)
+  DOCROOT=$(echo "${SITE}"| awk -F'app/code/core' {'print $1'})
+  
+  #Find Mage version of said site
+  VERSION=$(php -r ";require '${DOCROOT}/app/Mage.php'; echo Mage::getVersion(); "|tr -d'.')
 
-  if [${VERSION} -le 1500 ]
+  if [ ${VERSION} -le 1500 ]
   then
     PATCH=/tmp/magepatches/1.4.0-1.5.0.sh
-  elif [${VERSION} -ge 1510 -a ${VERSION} -le 1599]
+  elif [ ${VERSION} -ge 1510 -a ${VERSION} -le 1599 ]
   then
     PATCH=/tmp/magepatches/1.5.1.sh
-  elif [${VERSION} -ge 1600 -a ${VERSION} -le 1609]
+  elif [ ${VERSION} -ge 1600 -a ${VERSION} -le 1609 ]
   then
     PATCH=/tmp/magepatches/1.6.0.sh
-  elif [${VERSION} -ge 1610 -a ${VERSION} -le 1629]
+  elif [ ${VERSION} -ge 1610 -a ${VERSION} -le 1629 ]
   then
     PATCH=/tmp/magepatches/1.6.1-1.6.2.sh
-  elif [${VERSION} -ge 1700 -a ${VERSION} -le 1799]
+  elif [ ${VERSION} -ge 1700 -a ${VERSION} -le 1799 ]
   then
     PATCH=/tmp/magepatches/1.7.sh
-  elif [${VERSION} -ge 1800 -a ${VERSION} -le 1999]
+  elif [ ${VERSION} -ge 1800 -a ${VERSION} -le 1999 ]
   then
     PATCH=/tmp/magepatches/1.8-1.9.sh
   else
   echo -e "Can't determine a correct patch to apply, possibly no patch available for your magento version of ${VERSION}\n"
   fi
 
+#some issue lies here. When run, ${PATCH} is blank and DOCROOT is the entire content of /app/code/core/Mage/Core/Controller/Request/Http.php
+
   cp ${PATCH} ${DOCROOT}
   
-  PATCH=$(awk -F'/magepatches/' {'print $2'} ${PATCH})
+  PATCH=$(echo "${PATCH}" |awk -F'/magepatches/' {'print $2'})
 
   cd ${DOCROOT}
   OWNER=$(stat -c '%U' app/Mage.php)
